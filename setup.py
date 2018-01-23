540d5490e05a0383868d457b508a258ac455e9aa
Will Sheffler <willsheffler@gmail.com>
2018-01-23 15:51:15 -0800

    update dependencies

--- setup.py
+++ setup.py
@@ -12,17 +12,16 @@ with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
 requirements = [
-    # TODO: put package requirements here
+    'numpy'
 ]
 
 setup_requirements = [
     'pytest-runner',
-    # TODO(willsheffler): put setup requirements (distutils extensions, etc.) here
 ]
 
 test_requirements = [
     'pytest',
-    # TODO: put package test requirements here
+    'numpy',
 ]
 
 setup(
