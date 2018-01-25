948e272669d51336a0dd75c56e62d03223d839a8
Will Sheffler <willsheffler@gmail.com>
2018-01-24 16:40:53 -0800

    Bump version: 0.1.4 → 0.1.5

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ test_requirements = [
 
 setup(
     name='homog',
-    version='0.1.4',
+    version='0.1.5',
     description="Utilities for Homogeneous Coordinates with Numpy",
     long_description=readme + '\n\n' + history,
     author="Will Sheffler",
